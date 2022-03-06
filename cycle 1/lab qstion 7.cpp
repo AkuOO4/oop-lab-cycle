822ce2069d8e6afe7d0b9acdf2030a161ac8d72d
AkuOO4 <95350596+AkuOO4@users.noreply.github.com>
2022-03-06 23:33:23 +0530

    Add files via upload

--- cycle 1/lab qstion 7.cpp
+++ cycle 1/lab qstion 7.cpp
@@ -1,78 +1,146 @@
 #include <iostream>
 using namespace std;
 
-class matrix
-{
-	int c1[3], c2[3], c3[3];
-
+class matrices {
+	int r, c;
+	int matrix[10][10];
 public:
-	void show_matrix(void)
-	{
-		for (int i = 0; i < 3; i++)
-			cout << "|" << c1[i] << "," << c2[i] << "," << c3[i] << "|";
-	}
-
-	void put_matrix(void)
-	{
-		cout << "\n it is a 3x3 matrix\n";
-
-		cout << "enter row 1" << endl; 
-		for (int i = 0; i < 3; i++)
-					cin >> c1[i];
-		cout << "enter row 2" << endl;
-		for (int i = 0; i < 3; i++)
-			cin >> c2[i];
-		cout << "enter row 3" << endl;
-		for (int i = 0; i < 3; i++)
-			cin >> c3[i];
-
-		show_matrix();
-	}
-
-	void matrix_add(matrix t1, matrix t2);
-	void matrix_transpose(void);
-	void matrix_determinant(void);
-	
+	matrices() {};
+	void get_matrix(void);
+	friend void matrix_add(matrices, matrices);
+	friend void matrix_mult(matrices, matrices);
+	void matrix_transpose();
+	void matrix_trace();
 };
-void matrix::matrix_add(matrix t1, matrix t2)
+
+void matrices::get_matrix()
 {
-	matrix t3;
-	for (int i = 0; i < 3; i++)
+	cout << "Enter the No of Rows: ";
+	cin >> r;
+	cout << "Enter the No of Coloumns: ";
+	cin >> c;
+
+	for (int i = 0; i < r; ++i) 
 	{
-		t3.c1[i] = t1.c1[i] + t2.c1[i];
-		t3.c2[i] = t1.c2[i] + t2.c2[i];
-		t3.c3[i] = t1.c3[i] + t2.c3[i];
+		cout << "Enter elements of " << i + 1 << " row" << endl;
+		for (int j = 0; j < c; ++j)
+		{
+			cin >> matrix[i][j];
+		}
 	}
-	show_matrix();
 }
-
-void matrix::matrix_transpose()
+void matrix_add(matrices a, matrices b)
 {
-	int dupe1[3],dupe2[3],dupe3[3];
-	for (int i = 0; i < 3; i++)
-	{
-		dupe1[0] = c1[0];
-		dupe2[0] = c1[1];
-		dupe3[0] = c1[2];
-
-		dupe1[1] = c2[0];
-		dupe2[1] = c2[1];
-		dupe3[1] = c2[2];
-		dupe1[2] = c3[0];             
-		dupe2[2] = c3[1];
-		dupe3[2] = c3[2];
+	if (a.r == b.r and a.c == b.c) {
+		int sum[a.r][a.c];
+		for (int i = 0; i < a.r; ++i) {
+			for (int j = 0; j < a.c; ++j) {
+				sum[i][j] = a.matrix[i][j] + b.matrix[i][j];
+			}
+		}
+		for (int i = 0; i < a.r; ++i) {
+			for (int j = 0; j < b.c; ++j) {
+				cout << sum[i][j] << "\t";
+			}
+			cout << endl;
+		}
 	}
-	show_matrix();
+	else {
+		cout << "Addition not possible" << endl;
+	}
+
 }
 
-void matrix::matrix_determinant(void) //https://cdn1.byjus.com/wp-content/uploads/2018/11/maths/2015/12/24070546/Determinant-Of-A-Matrix.jpg
-{
-	int det = c1[0] * (c2[1] * c3[2] - c3[1] * c2[2]) + c2[0] * (c3[1] * c1[2] - c1[1] * c3[2]) + c3[0] * (c1[1] * c2[2] - c2[1] - c1[2]);
-	cout << "The determinent is " << det;
-}
-
-int main()
-{
+void matrices::matrix_transpose() {
+	int transpose[c][r];
+	//to take the transpose
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			transpose[j][i] = matrix[i][j];
+		}
+	}
 	
+	cout << "Transpose of the Matrix" << endl;
+	for (int i = 0; i < c; ++i) {
+		for (int j = 0; j < r; ++j) {
+			cout << transpose[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
 }
 
+void matrix_mult(matrices a, matrices b) {
+	if (a.c == b.r) {
+		int mult[a.r][b.c];
+		if (a.c == b.r) {
+			for (int i = 0; i < a.r; ++i) {
+				for (int j = 0; j < b.c; ++j) {
+					int sum = 0;
+					for (int k = 0; k < a.c; ++k) {
+						sum = sum + a.matrix[i][k] * b.matrix[k][j];
+						mult[i][j] = sum;
+					}
+				}
+			}
+			
+			cout << "matrix after multiplication" << endl;
+			for (int i = 0; i < a.r; ++i) {
+				for (int j = 0; j < b.c; ++j) {
+					cout << mult[i][j] << "\t";
+				}
+				cout << endl;
+			}
+		}
+	}
+	else {
+		cout << "matrix cannot be multiplied";
+	}
+
+}
+
+void matrices::matrix_trace() {
+	if (r != c) 
+		cout << "Trace is possible for square matrix";
+
+	else {
+		int trace = 0;
+		for (int i = 0; i < r; ++i) {
+			trace = trace + matrix[i][i];
+		}
+		cout << "Trace of the Matrix: " << trace;
+
+	}
+}
+int main() {
+	int choice;
+	matrices m1, m2;
+	cout << "1  Addition of Matrix\n2  Transpose of Matrix\n3 Matrix Multiplication\n4 Trace of Matrix" << "\n5 exit"<<endl;
+	cin >> choice;
+	m1.get_matrix();
+	m2.get_matrix();
+	while (choice != 5)
+	{
+		switch (choice) {
+		case 1:
+			matrix_add(m1, m2);
+			break;
+		case 2:
+			m1.matrix_transpose();
+			break;
+		case 3:
+			matrix_mult(m1, m2);
+			break;
+		case 4:
+			m1.matrix_trace();
+			break;
+		default:
+			cout << "wrong choice";
+			break;
+		}
+		cout << "1  Addition of Matrix\n2  Transpose of Matrix\n3 Matrix Multiplication\n4 Trace of Matrix" << "\n5 exit" << endl;
+		cin >> choice;
+	}
+
+	return 0;
+}
